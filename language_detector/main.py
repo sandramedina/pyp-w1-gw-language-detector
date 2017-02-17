5d35da138aa4d3620b01960c70bbfda56b42ebfe
Sandra Medina <smedina.sea@gmail.com>
2017-02-17 01:07:36 +0000

    initial solution

--- language_detector/main.py
+++ language_detector/main.py
@@ -4,5 +4,12 @@
 
 def detect_language(text, languages):
     """Returns the detected language of given text."""
-    # implement your solution here
-    pass
+    counter = 0
+    select_lang = None
+    for language in languages:
+        num_of_words = len([word for word in language['common_words'] if word in text])
+        if num_of_words > counter:
+            counter = num_of_words
+            select_lang = language['name'] 
+            
+    return select_lang
